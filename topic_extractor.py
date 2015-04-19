877a5359ca79768b5f8a6f888e0eb73cf3237ca5
Souleiman Ayoub <souleiman2010@gmail.com>
2015-04-19 00:22:53 -0400

    Added Topic Extraction

---
+++ topic_extractor.py
@@ -0,0 +1,36 @@
+__author__ = 'Souleiman Ayoub'
+
+import os
+import codecs
+
+from os import listdir
+from sys import argv
+
+# argv := topic_extractor.py [input_dir] [output_dir]
+
+if len(argv) != 3:
+    print('Invalid Parameters!')
+    print('topic_extractor.py [input_dir] [output_dir]')
+    exit(1)
+
+if __name__ == '__main__':
+    files = [file for file in listdir(argv[1]) if file.endswith('.csv')]
+
+    for f in files[:1]:
+        with codecs.open(os.path.join(argv[1], f), 'r', encoding='utf-8') as r:
+            contents = r.read().strip().split("\n")
+
+        header_count = int(len([x for x in contents[0].strip().split(",") if x]) / 2)
+        topics = [[] for x in range(header_count)]
+        for c in contents[1:]:  # Skip header
+            values = [x for x in c.strip().split(",") if x]  # Filter out empty strings in list
+
+            num_topics = int(len(values) / 2)
+            for i in range(0, num_topics):
+                topics[i].append(values[i * 2])
+        topic_model = {}
+        for i in range(0, header_count):
+            topic_model['Topic {}'.format(i)] = topics[i]
+
+        with codecs.open(os.path.join(argv[2], f).replace('.csv', '_alda.txt'), 'w', encoding='UTF-8') as w:
+            w.write(str(topic_model).replace("'", "\""))
